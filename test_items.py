398bb5fd18589a1a3c6ad665d11bea45ef87630b
Valeriya Vishnevskaya <valeriyavishnevskaya@MacBook-Pro-Valeriya.local>
2020-04-17 22:48:09 +0300

    language task2

--- test_items.py
+++ test_items.py
@@ -9,10 +9,15 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
+    # time.sleep(30)
+    basket_class = browser.find_element_by_css_selector("#add_to_basket_form > button")
+    basket = basket_class.get_attribute("class")
+    assert basket == "btn btn-lg btn-primary btn-add-to-basket", "Button does not found!"
+
+    # assert browser.find_element_by_css_selector(By.XPATH,"//button[@value='Add to basket']"),"Button doesn't exist"
     # поиск кнопки добавляни в корзину
-    basket_button = WebDriverWait(browser, 3).until(
-		EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "Button doesn't exist")
+    # basket_button = WebDriverWait(browser, 3).until(
+	# 	EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "Button doesn't exist")
 
 
 
